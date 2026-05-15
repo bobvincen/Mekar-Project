d8c13ba6f083358463ed61a21d543ff3e05dbf9b
bobvincen <ariskihabibi06@gmail.com>
2026-05-15 23:03:17 +0700

    refactor : merapikan model supplier

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Supplier extends Model
 {
-    //
+    protected $fillable = [
+        'nama_supplier',
+        'no_hp',
+        'alamat'
+    ];
 }
