f4994309c0eb83b4156cf6900ebb69b6d0c6b9b2
BilXxzz <nabilokta61537@gmail.com>
2026-06-11 11:23:11 +0700

    feat:Supplier dan HomeMP

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriController;
+use App\Http\Controllers\SupplierController;
 
 Route::get('/dashboard', [DashboardController::class, 'index']);
 Route::resource('kategori', KategoriController::class);
+Route::resource('supplier', SupplierController::class);
 Route::get('/', function () {
-    return view('welcome');
+    return view('marketplace.home');
 });
 
