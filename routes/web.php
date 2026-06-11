65655e8f8baee969716ba334c05a124c069e2512
bobvincen <ariskihabibi06@gmail.com>
2026-06-11 10:09:35 +0700

    Update setelah merge develop

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\ObatController;
+use App\Http\Controllers\TransaksiController;
 
 
 Route::get('/', [DashboardController::class, 'index']);
