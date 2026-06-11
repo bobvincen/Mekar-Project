37bc383d1709d6b9356f4d890bb42e61a4dcfe0d
bobvincen <ariskihabibi06@gmail.com>
2026-06-11 11:54:57 +0700

    feat : menambah laravel brezee untuk fitur login

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mekar Project</title>
+</head>
+<body>
+    <h1>Mekar Project</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\KategoriController;
-use App\Http\Controllers\ObatController;
-use App\Http\Controllers\TransaksiController;
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [DashboardController::class, 'index']);
-Route::get('/dashboard', [DashboardController::class, 'index']);
-Route::resource('kategori', KategoriController::class);
-Route::resource('obat', ObatController::class);
-Route::resource('transaksi', TransaksiController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-
-
+require __DIR__.'/auth.php';
